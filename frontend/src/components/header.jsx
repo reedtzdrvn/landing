eaee561268697e6803fc9c973a0028751ec497a1
Daniil Markov <reedtzhere@gmail.com>
2024-04-11 17:59:39 +0300

    changed text

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,39 +1,40 @@
-import './header.css'
+import "./header.css";
 
 const Header = () => {
-	return (
-		<>
-			<div className='Header'>
-				<div className='NavBar flex  h-[65px] bg-[#465373]'>
-					<div className='container text-[#E4E9EF] items-center flex justify-start gap-10 px-[64px]'>
-						<a className='cursor-pointer' href='#map'>
-							Карта
-						</a>
-						<a className='cursor-pointer' href='#form'>
-							Форма
-						</a>
-					</div>
-				</div>
-				<div className='Header_body flex justify-center items-center px-[128px]'>
-					<div className='container text-[#E4E9EF] relative'>
-						<div className='title_text font-bold text-3xl mb-10 text-justify'>
-							Lorem Ipsum - это текст-"рыба", часто используемый в печати и
-							вэб-дизайне.
-						</div>
-						<div className='lol flex justify-between items-end'>
-							<div className='w-[50%]'>
-								Lorem Ipsum - это текст-"рыба", часто используемый в печати и
-								вэб-дизайне. Lorem Ipsum - это текст-"рыба", часто используемый
-								в печати и вэб-дизайне. Lorem Ipsum - это текст-"рыба", часто
-								используемый в печати и вэб-дизайне. Lorem Ipsum - это
-								текст-"рыба", часто используемый в печати и вэб-дизайне.{' '}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	)
-}
+  return (
+    <>
+      <div className="Header">
+        <div className="NavBar flex  h-[65px] bg-[#465373]">
+          <div className="container text-[#E4E9EF] items-center flex justify-start gap-10 px-[64px]">
+            <a className="cursor-pointer" href="#map">
+              Карта
+            </a>
+            <a className="cursor-pointer" href="#form">
+              Форма
+            </a>
+          </div>
+        </div>
+        <div className="Header_body flex justify-center items-center px-[128px]">
+          <div className="container text-[#E4E9EF] relative">
+            <div className="title_text font-bold text-3xl mb-10 text-justify">
+              Анализ и прогноз стоимости квартир в Москве на вторичном рынке
+            </div>
+            <div className="flex justify-between items-end">
+              <div className="w-[50%]">
+                В ходе проекта мы проанализировали стоимость квартир в Москве на
+                вторичном рынке, используя методы машинного обучения. Объектом анализа являлась сборная
+                статистика о стоимости квартир, а предметом являлось построение
+                эконометрической модели. Как результат мы видим
+                лендинг-страницу, доступную абсолютно каждому и бота в
+                телеграме.{" "}
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
